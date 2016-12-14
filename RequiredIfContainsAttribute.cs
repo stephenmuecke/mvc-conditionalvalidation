ceff76047991b56f46673a45ed3e6de569d3144c
stephenmuecke <smuecke@optusnet.com.au>
2016-12-14 15:54:40 +1030

    Add files via upload

---
+++ RequiredIfContainsAttribute.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Sandtrap.Web.Validation
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class RequiredIfContainsAttribute : ValidationAttribute, IClientValidatable
+    {
+
+        #region .Declarations 
+
+        private const string _DefaultErrorMessage = "The {0} field is required";
+        private readonly string _DependentProperty;
+        private IEnumerable<string> _TargetValues { get; set; }
+
+        #endregion
+
+        #region .Constructors 
+
+        /// <summary>
+        /// Initializes a new RequiredIfContainsAttribute.
+        /// </summary>
+        /// <param name="dependentProperty">
+        /// 
+        /// </param>
+        /// <param name="targetValues">
+        /// 
+        /// </param>
+        public RequiredIfContainsAttribute(string dependentProperty, string targetValues)
+        {
+            if (string.IsNullOrEmpty(dependentProperty))
+            {
+                throw new ArgumentNullException("dependentProperty");
+            }
+            _DependentProperty = dependentProperty;
+            _TargetValues = targetValues.Split(',').Select(x => x.Trim().ToLower());
+            ErrorMessage = _DefaultErrorMessage;
+        }
+
+        #endregion
+
+        #region .Methods 
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="validationContext"></param>
+        /// <returns></returns>
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                // Get the value of the dependant property
+                var property = validationContext.ObjectInstance.GetType().GetProperty(_DependentProperty);
+                var propertyValue = property.GetValue(validationContext.ObjectInstance, null);
+                if (propertyValue != null && _TargetValues.Contains(propertyValue.ToString().ToLower()))
+                {
+                    return new ValidationResult(string.Format(ErrorMessageString, validationContext.DisplayName));
+                }
+            }
+            return ValidationResult.Success;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="metadata"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context)
+        {
+            var rule = new ModelClientValidationRule
+            {
+                ValidationType = "requiredifcontains",
+                ErrorMessage = FormatErrorMessage(metadata.GetDisplayName()),
+            };
+            rule.ValidationParameters.Add("dependentproperty", _DependentProperty);
+            rule.ValidationParameters.Add("targetvalues", string.Join(",", _TargetValues));
+            yield return rule;
+        }
+
+        #endregion
+
+
+    }
+}
---
+++ sandtrap-unobtrusive-validation-v2.0.js
@@ -0,0 +1,55 @@
+﻿sandtrapValidation = {
+    getDependantElement: function (validationElement, dependentProperty) {
+        var dependentElement = $('#' + dependentProperty);
+        if (dependentElement.length === 1) {
+            return dependentElement;
+        }
+        var name = validationElement.name;
+        var index = name.lastIndexOf(".") + 1;
+        var id = (name.substr(0, index) + dependentProperty).replace(/[\.\[\]]/g, "_");
+        dependentElement = $('#' + id);
+        if (dependentElement.length === 1) {
+            return dependentElement;
+        }
+        // Try using the name attribute
+        name = (name.substr(0, index) + dependentProperty);
+        dependentElement = $('[name="' + name + '"]');
+        if (dependentElement.length > 0) {
+            return dependentElement.first();
+        }
+        return null;
+    }
+}
+
+$.validator.addMethod("requiredifcontains", function (value, element, params) {
+    if ($(element).val() != '') {
+        // The element has a value so its OK
+        return true;
+    }
+    if (!params.dependentelement) {
+        return true;
+    }
+    var dependentElement = $(params.dependentelement);
+    var targetValues = params.targetvalues;
+    var dependentValue;
+    if (dependentElement.is(':radio')) {
+        // If its a radio button, we cannot rely on the id attribute
+        // So use the name attribute to get the value of the checked radio button
+        var dependentName = dependentElement[0].name;
+        dependentValue = $('input[name="' + dependentName + '"]:checked').val().toLowerCase();
+    } else {
+        dependentValue = dependentElement.val().toLowerCase();
+    }
+    return $.inArray(dependentValue, targetValues) === -1;
+});
+
+$.validator.unobtrusive.adapters.add("requiredifcontains", ["dependentproperty", "targetvalues"], function (options) {
+    var element = options.element;
+    var dependentproperty = options.params.dependentproperty;
+    var dependentElement = sandtrapValidation.getDependantElement(element, dependentproperty);
+    options.rules['requiredifcontains'] = {
+        dependentelement: dependentElement,
+        targetvalues: options.params.targetvalues.split(',')
+    };
+    options.messages['requiredifcontains'] = options.message;
+});
